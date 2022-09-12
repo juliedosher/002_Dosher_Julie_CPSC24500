6c6debf14c955b5d2194304c9128582654a5cae1
juliedosher <juliegdosher@lewisu.edu>
2022-09-11 23:43:58 -0500

    completed CircleCalc class

--- Labs/src/CircleCalc/CircleCalc.java
+++ Labs/src/CircleCalc/CircleCalc.java
@@ -3,6 +3,21 @@ package CircleCalc;
 public class CircleCalc {
 	
 	public static void main(String[] args) {
+		int randomRadius = (int)(Math.random() * 100);						// determines a random radius (integer) between 0 - 100
+		double circumference = calculateCircumference(randomRadius);
+		double area = calculateArea(randomRadius);
 		
+		System.out.println("Radius: " + randomRadius);
+		System.out.println("Circumference: " + circumference);
+		System.out.println("Area: " + area);
+	}
+	
+	
+	private static double calculateCircumference(int radius) {				// calculates circle's circumference 
+		return 2 * Math.PI * radius;
+	}
+	
+	private static double calculateArea(int radius) {						// calculates circle's area
+		return Math.PI * radius * radius;
 	}
 }
