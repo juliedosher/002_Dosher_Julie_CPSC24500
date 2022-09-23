68b8e8764c409bb54348c3773141a882de12171e
juliedosher <juliegdosher@lewisu.edu>
2022-09-23 11:12:26 -0500

    created printHeader method

---
+++ Labs/src/starlightcoffee/StarlightCoffee.java
@@ -0,0 +1,17 @@
+package starlightcoffee;
+
+public class StarlightCoffee {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	
+	private static void printHeading() {
+		System.out.println("*************************************");
+		System.out.println("STARLIGHT COFFEE POINT-OF-SALE SYSTEM");
+		System.out.println("*************************************\n");
+	}
+
+}
