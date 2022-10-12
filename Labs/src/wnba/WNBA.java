3cea4af12e59e9fb8ec8d957571fde30aefa656d
julie <jdosher2@illinois.edu>
2022-10-12 18:05:53 -0500

    started chooseOption() function

--- Labs/src/wnba/WNBA.java
+++ Labs/src/wnba/WNBA.java
@@ -1,12 +1,32 @@
 package wnba;
 
+import java.util.*;
+
 public class WNBA {
 
 	public static void main(String[] args) {
 		printHeading();
 
+		
+		
 	}
 	
+	private static int chooseOption(Scanner input) {
+		printOptions();
+		boolean isValidChoice = false;
+		int choice = 0;
+		
+		do {
+			int userIn = input.nextInt();
+			
+			
+		} while (!isValidChoice);
+		
+		
+		
+		
+		return 0;
+	}
 	
 	private static void printHeading() {
 		String asterisks = "*".repeat(51);
@@ -17,6 +37,14 @@ public class WNBA {
 		System.out.println(asterisks);
 		System.out.println(header);
 		System.out.println(asterisks);
+	}
+	
+	private static void printOptions() {
+		System.out.println("What would you like to see?");
+		System.out.println("1. Eastern Conference");
+		System.out.println("2. Western Conference");
+		System.out.println("3. Combined");
+		System.out.println("4. Exit");
 	}
 
 }
