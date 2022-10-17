02cf368813e22856910c816780d7c0d56e593b7e
julie <jdosher2@illinois.edu>
2022-10-17 11:48:31 -0500

    created file opening function

--- Labs/src/wnba/WNBA.java
+++ Labs/src/wnba/WNBA.java
@@ -1,32 +1,75 @@
 package wnba;
 
+import java.io.*;
 import java.util.*;
 
 public class WNBA {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		printHeading();
+		File file = getFile(input);
+		
+		if (checkIfFileExists(file, input)) {
+			// while loop
+			
+			
+		} else {
+			printExitMessage();
+		}
+			
+	}
+		
+		
+	
 
+	
+	private static File getFile(Scanner input) {
+		System.out.println("Enter the standings filename: ");
+		String fileName = input.nextLine();
+		File file = new File(fileName);
 		
-		
+		return file;
 	}
 	
-	private static int chooseOption(Scanner input) { 				// finish this later
+	private static boolean checkIfFileExists(File file, Scanner userInput) {
+		boolean fileExists = false;
+		
+		try {
+			Scanner scanFile = new Scanner(file);
+			fileExists = true;
+			
+		} catch (FileNotFoundException e) {
+			fileExists = false;
+		}
+		
+		return fileExists;
+	}
+	
+	private static int chooseOption(Scanner input) { 
 		printOptions();
 		boolean isValidChoice = false;
 		int choice = 0;
 		
 		do {
-			int userIn = input.nextInt();
+			try {
+				choice = input.nextInt();
+			} catch (IllegalArgumentException e) {
+				System.out.println("\nThat is an invalid choice.\n");
+			}
 			
+			if (choice >= 1 && choice <= 4) {
+				isValidChoice = true;
+			} else {
+				System.out.println("\nThat is an invalid choice.\n");
+			}
 			
 		} while (!isValidChoice);
 		
 		
-		
-		
-		return 0;
+		return choice;
 	}
+	
 	
 	private static void printHeading() {
 		String asterisks = "*".repeat(51);
@@ -47,4 +90,7 @@ public class WNBA {
 		System.out.println("4. Exit");
 	}
 
+	private static void printExitMessage() {
+		System.out.println("File does not exist. Exiting program");
+	}
 }
