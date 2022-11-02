a7eded6e7529b22e519bc9eb08c5266e606be15c
julie <jdosher2@illinois.edu>
2022-11-02 11:47:03 -0500

    finished WordFileReader class

--- Labs/src/storyteller/WordFileReader.java
+++ Labs/src/storyteller/WordFileReader.java
@@ -5,28 +5,46 @@ import java.util.*;
 
 class WordFileReader {
 
-	public static ArrayList<String> readFile(String fileName) {
+	public static HashMap<String, ArrayList<String>> readFile(String fileName) {					// reads through file and returns a HashMap of
+		HashMap<String, ArrayList<String>> dictionary = new HashMap<String, ArrayList<String>>();	// all words mapped to the type of word it is
+		ArrayList<String> adjectives = new ArrayList<String>();
+		ArrayList<String> adverbs = new ArrayList<String>();
+		ArrayList<String> prepositions = new ArrayList<String>();
+		ArrayList<String> nouns = new ArrayList<String>();
+		ArrayList<String> verbs = new ArrayList<String>();
+		
+		dictionary.put("adj", adjectives);
+		dictionary.put("adv", adverbs);
+		dictionary.put("prep", prepositions);
+		dictionary.put("n", nouns);
+		dictionary.put("v", verbs);
 		
 		try {
 			Scanner fileScan = new Scanner(new File(fileName));
-			
 				
 			String word = "";
 			String type = "";
 			while (fileScan.hasNextLine()) {
 				word = fileScan.next();
 				type = fileScan.next();
+			
+				if (dictionary.containsKey(type)) {
+					ArrayList<String> wordList = dictionary.get(type);
+					wordList.add(word);
+					dictionary.replace(type, wordList);
+				}
 			}
-				
 			
-			
-		} catch (FileNotFoundException e) {
-			
+		} catch (FileNotFoundException e) {															// in case file is not found
+			printFileError();
 		}
-			
 		
-		return null;
-		
+		return dictionary;
 	}
 	
+	
+	private static void printFileError() {
+		System.out.println("ERROR: That file was not found.");
+		System.out.println();
+	}
 }
