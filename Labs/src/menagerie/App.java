3df84320dc19638dd0612d51a013bbb9c39c6c84
julie <jdosher2@illinois.edu>
2022-11-03 18:32:23 -0500

    worked on some App.java functions for some basic functionality

--- Labs/src/menagerie/App.java
+++ Labs/src/menagerie/App.java
@@ -1,14 +1,88 @@
 package menagerie;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		
+		printHeading();
+		printMenu();
+		getPet(input);
 
 	}
 
 	
+	private static int getChoice(Scanner input) {
+		int choice = 0;
+		
+		boolean isValid = false;
+		while (!isValid) {
+			System.out.print("Enter the number of your choice: ");
+			int num = input.nextInt();
+			if (num >= 1 && num <= 7) {
+				choice = num;
+				isValid = true;
+				
+			} else {
+				printInvalidInput();
+			}
+		}
+		
+		input.nextLine();
+		return 0;
+	}
 	
+	private static Pet getPet(Scanner input) {
+		//Pet pet;
+		String animal = "";
+		String name = "";
+		int age = 0;
+		int weight = 0;
+		
+		boolean validInput = false;
+		while (!validInput) {
+			System.out.print("Enter d for dog, c for cat, or f for fish: ");
+			String choice = input.nextLine();
+			choice = choice.toLowerCase();
+			if (choice.equals("d")) {
+				animal = "dog";
+				validInput = true;
+				
+			} else if (choice.equals("c")) {
+				animal = "cat";
+				validInput = true;
+				
+			} else if (choice.equals("f")) {
+				animal = "fish";
+				validInput = true;
+				
+			} else {
+				printInvalidInput();
+			}
+		}
+		
+		validInput = false;
+		while (!validInput) {
+			System.out.print("Enter name, age, and weight: ");
+			name = input.next();
+			try {
+				age = input.nextInt();
+				weight = input.nextInt();
+			} catch (Exception e) {
+				printInvalidInput();
+			}
+			validInput = true;
+		}
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(weight);
+		System.out.println(animal);
+		
+		return null;
+	}
 	
 	private static void printHeading() {
 		String asterisks = "*".repeat(60);
@@ -32,6 +106,11 @@ public class App {
 		System.out.println("5. Simulate a day in the life of your pets.");
 		System.out.println("6. Clear your list of pets.");
 		System.out.println("7. Exit");
+	}
+	
+	private static void printInvalidInput() {
+		System.out.println("Invalid input. Please try again.");
+		System.out.println();
 	}
 	
 }
