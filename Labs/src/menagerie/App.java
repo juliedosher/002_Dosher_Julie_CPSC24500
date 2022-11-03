7476b8b553dc49204e5a1d308750876aacd97876
julie <jdosher2@illinois.edu>
2022-11-03 18:13:25 -0500

    created print functions

--- Labs/src/menagerie/App.java
+++ Labs/src/menagerie/App.java
@@ -3,8 +3,35 @@ package menagerie;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
+	
+	
+	
+	private static void printHeading() {
+		String asterisks = "*".repeat(60);
+		String mainHeading = "Menagerie V1.0: The Pet Simulator";
+		String space = " ".repeat((asterisks.length() - mainHeading.length()) / 2);
+		
+		System.out.println(asterisks);
+		System.out.print(space);
+		System.out.print(mainHeading);
+		System.out.println(space);
+		System.out.println(asterisks);
+		System.out.println();
+	}
+	
+	private static void printMenu() {
+		System.out.println("What would you like to do?");
+		System.out.println("1. Add a new pet.");
+		System.out.println("2. Print list of pets.");
+		System.out.println("3. Save pets to a file.");
+		System.out.println("4. Load pets from a file.");
+		System.out.println("5. Simulate a day in the life of your pets.");
+		System.out.println("6. Clear your list of pets.");
+		System.out.println("7. Exit");
+	}
+	
 }
