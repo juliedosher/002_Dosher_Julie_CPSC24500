65abf0d7d05846906e4b0adb4b61c0db4b3674af
julie <jdosher2@illinois.edu>
2022-11-10 18:58:40 -0600

    added Pet methods

--- Labs/src/menagerie/Pet.java
+++ Labs/src/menagerie/Pet.java
@@ -2,4 +2,15 @@ package menagerie;
 
 public abstract class Pet {
 
+	String name;
+	int age;
+	double weight;
+	
+	public abstract String getType();
+	
+	public String toString() {													//add proper spacing
+		String output = this.getType() + name + age + weight;
+		return output;
+	}
+	
 }
