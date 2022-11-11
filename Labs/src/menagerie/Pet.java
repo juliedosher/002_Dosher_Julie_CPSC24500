660ae466fdbf2471e6ca5d961e6228b6cc63285a
julie <jdosher2@illinois.edu>
2022-11-10 19:06:11 -0600

    added to Pet class

--- Labs/src/menagerie/Pet.java
+++ Labs/src/menagerie/Pet.java
@@ -1,16 +1,50 @@
 package menagerie;
 
 public abstract class Pet {
-
+																				// add random number generator
 	String name;
 	int age;
 	double weight;
 	
+	int foodCutoff;
+	int attentionCutoff;
+	int sleepCutoff;
+	
 	public abstract String getType();
 	
-	public String toString() {													//add proper spacing
+	public abstract void setFoodCutoff(int cutoff);
+	public abstract void setAttentionCutoff(int cutoff);
+	public abstract void setSleepCutoff(int cutoff);
+	
+	public String toString() {													// finish!
 		String output = this.getType() + name + age + weight;
 		return output;
 	}
 	
+	public boolean needsFood(int food) {
+		boolean doesNeed = false;
+		
+		if (food < foodCutoff) {
+			doesNeed = true;
+		}
+		return doesNeed;
+	}
+	
+	public boolean needsAttention(int attention) {
+		boolean doesNeed = false;
+		
+		if (attention < attentionCutoff) {
+			doesNeed = true;
+		}
+		return doesNeed;
+	}
+	
+	public boolean needsSleep(int sleep) {
+		boolean doesNeed = false;
+		
+		if (sleep < sleepCutoff) {
+			doesNeed = true;
+		}
+		return doesNeed;
+	}
 }
