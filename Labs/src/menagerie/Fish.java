8c6b2f2123170a5a686b8d0733ab653a829bf637
julie <jdosher2@illinois.edu>
2022-11-14 11:17:43 -0600

    added constructor

--- Labs/src/menagerie/Fish.java
+++ Labs/src/menagerie/Fish.java
@@ -2,6 +2,12 @@ package menagerie;
 
 public class Fish extends Pet {
 
+	public Fish(String name, int age, double weight) {
+		this.name = name;
+		this.age = age;
+		this.weight = weight;
+	}
+	
 	@Override
 	public String getType() {
 		return "fish";
