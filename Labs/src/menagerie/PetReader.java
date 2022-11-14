f16855ee52971932c12dc01cf20e58409206cfb6
julie <jdosher2@illinois.edu>
2022-11-14 11:20:43 -0600

    finished PetReader class

--- Labs/src/menagerie/PetReader.java
+++ Labs/src/menagerie/PetReader.java
@@ -18,11 +18,29 @@ public class PetReader {
 			
 			Scanner fileScan = new Scanner(new File(fileName));
 				
-			while (fileScan.hasNextLine()) {
-				type = fileScan.next();										// CHANGE! parse between tabs ********
-				name = fileScan.next();
-				age = fileScan.nextInt();
-				weight = fileScan.nextDouble();
+			while (fileScan.hasNextLine()) {														// gets info from .txt file one line 
+				String[] split = fileScan.nextLine().split("\t");									// at a time
+				type = split[0];										
+				name = split[1];
+				age = Integer.parseInt(split[2]);
+				weight = Double.parseDouble(split[3]);
+				
+				switch (type) {																		// creates new pet object depending on
+					case "dog":																		// pet type
+						Dog newDog = new Dog(name, age, weight);
+						pets.add(newDog);
+						break;
+						
+					case "cat":
+						Cat newCat = new Cat(name, age, weight);
+						pets.add(newCat);
+						break;
+						
+					case "fish":
+						Fish newFish = new Fish(name, age, weight);
+						pets.add(newFish);
+						break;
+				}
 			}
 			
 		} catch (FileNotFoundException e) {															// should never be used because this
