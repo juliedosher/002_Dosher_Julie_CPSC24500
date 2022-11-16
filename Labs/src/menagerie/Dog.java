89d26c752c026ffac27285c61bcb85fa1cb9aaa0
juliedosher <juliegdosher@lewisu.edu>
2022-11-16 15:10:23 -0600

    added actions

--- Labs/src/menagerie/Dog.java
+++ Labs/src/menagerie/Dog.java
@@ -10,19 +10,28 @@ public class Dog extends Pet {
 		this.name = "";
 		this.age = 0;
 		this.weight = 0;
-		this.foodCutoff = 3;
+		this.foodCutoff = 4;
 		this.attentionCutoff = 8;
 		this.sleepCutoff = 8;
-		this.actions = new String[] {""};
+		this.actions = new String[] {"barked at the window.", "whimpered.",
+				"tore the squeaker out of its toy.", "played fetch.", 
+				"wagged its tail.", "tore up a pillow.", "drank some water.", 
+				"chased its tail.", "played with its chew toy.", "ran around.", 
+				"hid one of your shoes.", "jumped on the couch."};
 	}
 	
 	public Dog(String name, int age, double weight) {								// constructor
 		this.name = name;
 		this.age = age;
 		this.weight = weight;
-		this.foodCutoff = 3;
+		this.foodCutoff = 4;
 		this.attentionCutoff = 8;
 		this.sleepCutoff = 8;
+		this.actions = new String[] {"barked at the window.", "whimpered.",
+				"tore the squeaker out of its toy.", "played fetch.", 
+				"wagged its tail.", "tore up a pillow.", "drank some water.", 
+				"chased its tail.", "played with its chew toy.", "ran around.", 
+				"hid one of your shoes."};
 	}
 	
 	
