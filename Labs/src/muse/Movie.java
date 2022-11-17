9c5aa4a9c61471ec6f355759bbe2f841e0f5012a
julie <jdosher2@illinois.edu>
2022-11-17 11:03:02 -0600

    added comments

--- Labs/src/muse/Movie.java
+++ Labs/src/muse/Movie.java
@@ -1,3 +1,7 @@
+/*
+ * The class that defines Movie, inherited from RecordedArtisticWork
+ */
+
 package muse;
 
 import java.util.LinkedHashMap;
@@ -21,27 +25,27 @@ public class Movie extends RecordedArtisticWork {
     public void setResolution(String res) {
         resolution = res;
     }
-    public Movie() {
+    public Movie() {																				// default constructor
         frameRate = 30;
         resolution = "720p";
     }
-    public Movie(String creator, String date, String title, String description,
+    public Movie(String creator, String date, String title, String description,						// constructor using individual data parts
             int duration, String fileName, double fileSize, int frameRate, String resolution) {
         super(creator,date,title,description,duration,fileName,fileSize);
         setFrameRate(frameRate);
         setResolution(resolution);
     }
-    public Movie(LinkedHashMap<String,String> settings) {
+    public Movie(LinkedHashMap<String,String> settings) {											// constructor using Linked Hash Map of all data
        super(settings);
        setFrameRate(Integer.parseInt(settings.get("framerate")));
        setResolution(settings.get("resolution"));
     }
     @Override
-    public String getType() {
+    public String getType() {																		// returns media type as  a String
         return "movie";
     }
     @Override
-    public String getSpecificInfoString() {
+    public String getSpecificInfoString() {															// returns data as nicely formatted String
         return String.format("filmed with %s resolution at %d frames per second", resolution, frameRate);
     }
 }
