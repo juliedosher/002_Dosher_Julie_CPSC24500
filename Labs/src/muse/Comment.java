2c8f51fe038e66d6d1f5466339a8803f0d67a012
julie <jdosher2@illinois.edu>
2022-11-17 11:01:09 -0600

    added comments

--- Labs/src/muse/Comment.java
+++ Labs/src/muse/Comment.java
@@ -1,3 +1,7 @@
+/*
+ * The class that defines Comment
+ */
+
 package muse;
 
 public class Comment {
@@ -7,33 +11,34 @@ public class Comment {
     public String getPostedBy() {
         return postedBy;
     }
-    public void setPostedBy(String postedBy) {
+    public void setPostedBy(String postedBy) {										// sets author of post
         this.postedBy = postedBy;
     }
-    public String getDateTime() {
+    public String getDateTime() {													// gets date/time post was made
         return dateTime;
     }
-    public void setDateTime(String dateTime) {
+    public void setDateTime(String dateTime) {										// sets date/time post was made
         this.dateTime = dateTime;
     }
-    public String getContent() {
+    public String getContent() {													// gets content of comment
         return content;
     }
-    public void setContent(String content) {
+    public void setContent(String content) {										// sets content of comment
         this.content = content;
     }
-    public Comment() {
+    public Comment() {																// default constructor
         postedBy = "";
         dateTime = "";
         content = "";
     }
-    public Comment(String postedBy, String dateTime, String content) {
+    public Comment(String postedBy, String dateTime, String content) {				// constructor
         setPostedBy(postedBy);
         setDateTime(dateTime);
         setContent(content);
     }
     @Override
-    public String toString() {
-        return String.format("On %s, %s commented ...\n\t\"%s\"", dateTime, postedBy, content);
+    public String toString() {														// returns comment info as a String
+        return String.format("On %s, %s commented ...\n\t\"%s\"", 	
+        		dateTime, postedBy, content);
     }
 }
