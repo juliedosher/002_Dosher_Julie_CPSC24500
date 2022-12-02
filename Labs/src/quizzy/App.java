a4345767d990eab99c21549e518ffee30f620913
juliedosher <juliegdosher@lewisu.edu>
2022-12-02 11:55:15 -0600

    added quiz answers to top of App.java

--- Labs/src/quizzy/App.java
+++ Labs/src/quizzy/App.java
@@ -1,9 +1,27 @@
+/*
+ * Quiz Answers:
+ * 1. C
+ * 2. C
+ * 3. A
+ * 4. B
+ * 5. B
+ * 6. D
+ * 7. C
+ * 8. A
+ * 9. C
+ * 10. B
+ * 11. A
+ * 12. C
+ * 13. D
+ * 14. D
+ */
+
 package quizzy;
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
