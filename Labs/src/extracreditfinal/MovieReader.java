fb68d2223295f1ad04d83e7cf1af9dd0fe657319
julie <jdosher2@illinois.edu>
2022-12-06 17:45:50 -0600

    updated comments

--- Labs/src/extracreditfinal/MovieReader.java
+++ Labs/src/extracreditfinal/MovieReader.java
@@ -5,24 +5,24 @@ import java.util.*;
 
 public class MovieReader {
 	
-	public static ArrayList<Movie> getMoviesFromTextFile(File file) {											// reads text file and returns ArrayList 
-		ArrayList<Movie> movies = new ArrayList<Movie>();											// of questions
+	public static ArrayList<Movie> getMoviesFromTextFile(File file) {								// reads text file and returns ArrayList 
+		ArrayList<Movie> movies = new ArrayList<Movie>();											// of movies
 		
 		try {
 			Scanner fileScan = new Scanner(file);
 		
 			while (fileScan.hasNextLine()) {													
-				String[] split = fileScan.nextLine().split("\t");											// gets question and answer parts from 
-				String title = split[0];																// each line
+				String[] split = fileScan.nextLine().split("\t");									// gets movie parts from each line
+				String title = split[0];															
 				int year = Integer.valueOf(split[1]);
 				String director = split[2];
 				int runtime = Integer.valueOf(split[3]);
 				String description = split[4];
 				
 				Movie movie = new Movie(title, year, director, runtime, description);
-				movies.add(movie);																	// adds new question to ArrayList
+				movies.add(movie);																	// adds new movie to ArrayList
 			}
-			fileScan.close();																				// closes file
+			fileScan.close();																		// closes file
 				
 		} catch (FileNotFoundException e) {
 			System.out.println("ERROR: File not found.");
