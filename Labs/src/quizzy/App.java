cfbaca6a61c6c178713fda59a5ded8d50047477e
julie <jdosher2@illinois.edu>
2022-12-06 17:09:57 -0600

    started implementing the GUI

--- Labs/src/quizzy/App.java
+++ Labs/src/quizzy/App.java
@@ -19,13 +19,35 @@
 package quizzy;
 
 import java.util.*;
+import javax.swing.*;
+import java.awt.*;
 
 public class App {
 
 	public static void main(String[] args) {
 		ArrayList<Question> questions = QuestionReader.getQuizFromFile(null); 	// TODO: get file name
 		
-
+		JFrame frame = new JFrame();
+		frame.setTitle("Object-Oriented Quiz Tool");
+		Container c = frame.getContentPane();
+		c.setLayout(new BorderLayout());
+		
+		JPanel mainPanel = new JPanel();
+		
+		JButton btnSubmit = new JButton("Submit answer");
+		JButton btnNext = new JButton("Next question");
+		JLabel labWelcome = new JLabel("Welcome to Quizzy, the object-oriented programming quiz tool.\n"
+				+ "Select File >> Load Questions to begin.");
+		
+		mainPanel.add(labWelcome);
+		c.add(mainPanel, BorderLayout.NORTH);
+		c.add(btnSubmit, BorderLayout.CENTER);
+		c.add(btnNext, BorderLayout.EAST);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 400, 400);
+		frame.setVisible(true);
+		
 	}
 	
 }
