7fbb14c6c1b38e9603e44749dcf22f22cfd7cef6
julie <jdosher2@illinois.edu>
2022-12-05 18:43:24 -0600

    created Movie class

--- Labs/src/extracreditfinal/Movie.java
+++ Labs/src/extracreditfinal/Movie.java
@@ -2,4 +2,93 @@ package extracreditfinal;
 
 public class Movie {
 
+	Movie() {																								// default constructor
+		title = "";
+		releaseYear = 0;
+		director = "";
+		runtimeMinutes = 0;
+		description = "";
+	}
+	
+	Movie(String title, int releaseYear, String director, int runtimeMinutes, String description) {			// constructor
+		this.title = title;
+		this.releaseYear = releaseYear;
+		this.director = director;
+		this.runtimeMinutes = runtimeMinutes;
+		this.description = description;
+	}
+	
+	private String title;
+	private int releaseYear;
+	private String director;
+	private int runtimeMinutes;
+	private String description;
+	
+	
+	public String runtimeToString() {
+		String output = "";
+		if (runtimeMinutes < 60) {
+			output = runtimeMinutes + " min";
+		
+		} else {
+			int hours = runtimeMinutes / 60;
+			int minutes = runtimeMinutes % 60;
+			output += hours + " hr";
+			if (minutes > 0) {
+				output += ", " + minutes + " min";
+			}
+		}
+		
+		return output;
+	}
+	
+	public String toString() {
+		String output = title + ", " + releaseYear + ", ";
+		output += "dir. by " + director + ".";
+		output += runtimeToString() + ".\n";
+		output += description + "\n";
+		
+		return output;
+	}
+	
+	public String getTitle() {																				// getters and setters
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(int releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public int getRuntimeMinutes() {
+		return runtimeMinutes;
+	}
+
+	public void setRuntimeMinutes(int runtimeMinutes) {
+		this.runtimeMinutes = runtimeMinutes;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
