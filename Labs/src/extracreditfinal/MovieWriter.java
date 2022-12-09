24fb36a72f07886a9ee727b435978deabd056dd2
juliedosher <juliegdosher@lewisu.edu>
2022-12-09 14:12:05 -0600

    added functionality for writing to text file

--- Labs/src/extracreditfinal/MovieWriter.java
+++ Labs/src/extracreditfinal/MovieWriter.java
@@ -1,5 +1,51 @@
 package extracreditfinal;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MovieWriter {
 
+	public static String createFile(Scanner input) {								// takes in a String fileName and creates the file
+		boolean validInput = false;
+		String fileName = "";
+		
+		while (!validInput) {
+			System.out.print("Enter name of file to save: ");
+			fileName = input.nextLine();
+			
+			try {
+			      File newFile = new File(fileName);
+			      if (newFile.createNewFile()) {
+			        validInput = true;
+			     
+			      } else {
+			        // file exists error
+			      }
+			      
+			    } catch (IOException ex) {
+			      // file error
+			    }
+		}
+		
+		return fileName;
+	}
+	
+	public static void writeToTextFile(String fileName, ArrayList<Movie> movies) {
+		try {
+			FileWriter movieWriter = new FileWriter(fileName);
+		     
+			for (Movie movie : movies) {
+				movieWriter.write(movie.toTabbedString());
+				movieWriter.write("\n");
+			}
+		     movieWriter.close();														// closes file
+		     
+		} catch (IOException ex) {														// should never happen since this is already
+			// file error																// checked in createFile()
+		}
+	}
+	
 }
