5817537c89de517c465d2d80ebf904399687b3d6
juliedosher <juliegdosher@lewisu.edu>
2022-12-09 14:19:37 -0600

    added readBinaryFile()

--- Labs/src/extracreditfinal/MovieReader.java
+++ Labs/src/extracreditfinal/MovieReader.java
@@ -30,4 +30,23 @@ public class MovieReader {
 		
 		return movies;
 	}
+	
+	@SuppressWarnings("unchecked")
+	public static ArrayList<Movie> readBinaryFile(String fileName) {								// reads a binary file and returns 
+		ArrayList<Movie> movies = new ArrayList<Movie>();											// an ArrayList of Artistic Works
+		try {
+			FileInputStream fis = new FileInputStream(new File(fileName));
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			movies = (ArrayList<Movie>)ois.readObject();
+			
+			ois.close();
+			
+		} catch (IOException ex) {
+			// file error
+		} catch (ClassNotFoundException ex) {
+			// translation error
+		}
+		
+		return movies;
+	}
 }
